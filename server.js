962294538bc6a5e7f0461fc02fa8378de853f0ea
svenradic <radic.sven@gmail.com>
2025-04-10 15:37:55 +0200

    server ready for deploy

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Pozdrav sa Railway servera!");
+});
+app.listen(PORT, () => {
+  console.log("Server pokrenut na portu ${PORT}");
+});
